5ce2b53f8ad975fc3b2157dcf8246d7a30f52707
erdiizgi <erdi_izgi@hotmail.com.tr>
2015-04-05 18:58:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ei/merdivan/Observation.java
@@ -0,0 +1,16 @@
+package com.example.ei.merdivan;
+
+/**
+ * Created by EI on 4/5/2015.
+ */
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class Observation extends ActionBarActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.observation_create);
+    }
+}
---
+++ app/src/main/java/com/example/ei/merdivan/ObservationDetail.java
@@ -0,0 +1,15 @@
+package com.example.ei.merdivan;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by EI on 4/5/2015.
+ */
+public class ObservationDetail extends ActionBarActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.observation_detail);
+    }
+}
